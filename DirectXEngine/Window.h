55bef9cee50eba718c29cd0c1daf69c06b39351d
hollow-d9d <taronhar@gmail.com>
2022-10-11 00:51:00 +0400

    ADD raw window creation

---
+++ DirectXEngine/Window.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <windows.h>
+#include <iostream>
+
+class Window
+{
+	public:
+		Window();
+		//Initialize the window
+		bool init();
+		bool broadcast();
+		//Release the window
+		bool release();
+
+		bool isRun();
+
+		//EVENTS
+		virtual void onCreate() = 0;
+		virtual void onUpdate() = 0;
+		virtual void onDestroy();
+
+
+		~Window();
+	protected:
+		HWND m_hwnd;
+		bool m_is_run;
+};
+
